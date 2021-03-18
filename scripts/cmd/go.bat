990447aa6ccf3a43851ec82df847bfd6c7bbebc1
NeilMacMullen <neil.macmullen@outlook.com>
2021-03-18 11:10:52 +0000

    Update comment

--- scripts/cmd/go.bat
+++ scripts/cmd/go.bat
@@ -3,6 +3,6 @@ REM extrememly hacky way to cd by constructing a batch file
 REM which contains the CD command!
 SET scr="%TEMP%\go.bat"
 echo|set /p="cd " > %scr%
-REM we need to use this odd format to ensure that we change directory
+REM we need to use this odd format to ensure that we change drive as well as directory
 jumpfs.exe find --name %1 --format "%%f%%N%%D:" >> %scr%
 call %scr%
